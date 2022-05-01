5ae01e667adddd3ca4e0e3b3fb9846360a9e2c0d
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-05-01 19:28:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function showMessage () {
+    let userName = prompt('Как тебя зовут?', "друг");
+    alert(`Привет, ${userName}!`);
+}
+
+function sum () {
+    let a = prompt('Введите первое слагаемое');
+    let b = prompt('Введите второе слагаемое');
+    alert(`Сумма равна ${+a + +b}`);
+}
+
+function sub () {
+    let a = prompt('Введите уменьшаемое');
+    let b = prompt('Введите вычитаемое');
+    alert(`Разность равна ${a - b}`);
+}
+
+function mult () {
+    let a = prompt('Введите первый множитель');
+    let b = prompt('Введите второй множитель');
+    alert(`Произведение равно ${a * b}`);
+}
+
+function div () {
+    let a = prompt('Введите делимое');
+    let b = prompt('Введите делитель');
+    alert(`Частное равно ${a / b}`);
+}
